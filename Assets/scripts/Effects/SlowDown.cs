d22292eb7e7702459f6e491a4e1af7567829d09e
mrshmid <mrshmid@gmail.com>
2018-05-07 00:16:58 -0700

    going to redo it so that explosives just run a script on anything in the explosion radius instead of a hard-coded "make things die"

---
+++ Assets/scripts/Effects/SlowDown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDown : ExplosionEffect {
+
+    //Percentage from 0-100.  100 means total freeze.
+    public float slowAmount;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void Effect()
+    {
+        
+    }
+}
---
+++ Assets/scripts/ExplosionEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Explodable))]
+public abstract class ExplosionEffect : MonoBehaviour {
+
+    public Explodable explodable;
+
+	// Use this for initialization
+	void Start () {
+        explodable = GetComponent<Explodable>();
+	}
+
+    public abstract void Effect();
+}
