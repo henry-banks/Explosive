d94dafaf8a58d3c8eab8aeb48d324b4a563f8bc7
henry-banks <henry.banks@students.aie.edu.au>
2018-05-30 17:01:19 -0700

    add another victory didnt implement

--- Assets/scripts/managers and stuff/LevelManager.cs
+++ Assets/scripts/managers and stuff/LevelManager.cs
@@ -19,19 +19,43 @@ public class ObjLevelData
 
 public class LevelManager : MonoBehaviour {
 
-    public VictoryScript victory;
+    public List<VictoryScript> victories;
     public List<ObjLevelData> placeableObjects;
+    public bool customVictories = false;
+
+    public bool isWin { get {
+            foreach (VictoryScript v in victories)
+            { if (!v.isVictory) return false; }
+            return true;
+                } }
 
 	// Use this for initialization
 	void Start () {
         GameManager.Instance.levelManager = this;
         for (int i = 0; i < placeableObjects.Count; i++)
             placeableObjects[i].idx = i;
+
+        //If we're not using custom victories, assume that all the victoryScripts attached to the levelEditor are all the victories we want.
+        if (!customVictories)
+        {
+            victories.Clear();
+            foreach (VictoryScript v in GetComponents<VictoryScript>())
+                victories.Add(v);
+        }
 	}
 	
+    //Delayed victory check so we aren't running EVERY TICK.
+    private IEnumerator delayVictoryCheck()
+    {
+        if (isWin)
+            Victory();
+
+        yield return new WaitForSeconds(0.2f);
+    }
+
 	// Update is called once per frame
 	void Update () {
-        StartCoroutine(victory.VictoryCheck());
+        
 	}
 
 
